853761df6f7f59aca09957e4a3fcc4b2b15a9dd5
Daanrox <daanielrox@hotmail.com>
2023-03-17 15:37:46 -0300

    links roberta

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleExtensionImages() {
+    var extensionImages = document.getElementById("extension-images");
+    if (extensionImages.style.display === "none") {
+      extensionImages.style.display = "block";
+    } else {
+      extensionImages.style.display = "none";
+    }
+  }
+
+
+  
