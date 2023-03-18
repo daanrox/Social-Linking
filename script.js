23e295c08d59648d3d6aee8c8cdba090d3d91c30
Daanrox <daanielrox@hotmail.com>
2023-03-17 21:30:03 -0300

    add micro

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ function toggleExtensionImages() {
   }
 
 
-  
+  function toggleMicroImages() {
+    var microImages = document.getElementById("micro-images");
+    if (microImages.style.display === "none") {
+      microImages.style.display = "block";
+    } else {
+      microImages.style.display = "none";
+    }
+  }
+
+
